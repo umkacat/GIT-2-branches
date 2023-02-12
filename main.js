417b57d40fe3d7f079027ee6ac3cc04760816ac1
umkacat <hobbityanka@mail.ru>
2023-02-13 00:51:11 +0500

    notes

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 import {maxInArray} from "./maxInArray.js"
+import {factorial1} from "./factorial.js"
 
-let userArray = prompt('Введите числа через запятую').split(' ')
+let userChoise = prompt('Введите 1 - для maxInArray, 2 - для factorial')
+
+if (userChoise == 1) {
+
+    let userArray = prompt('Введите числа через запятую').split(' ')
     
-maxInArray(userArray)
+    maxInArray(userArray)
+}
+
+if (userChoise == 2) {
+
+    let factNum = prompt('Введите число')
+
+    factorial1(factNum)
+}
+
+else {
+    alert('stop')
+}
